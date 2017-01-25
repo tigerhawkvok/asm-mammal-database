87931e4f289ca71c6bc3e8f43825b599ef1e029f
Philip Kahn <tigerhawkvok@gmail.com>
2017-01-25 10:10:23 -0800

    Basic renames

--- debug_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-# Debugging
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-#include("admin-login.php");
-include("admin/login.php");
-
-?>
--- debug_api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-# Debugging
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-include("commonnames_api.php");
-
-?>

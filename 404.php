3f792aa81ef56e9b7c67534a95c021009504a268
Philip Kahn <tigerhawkvok@gmail.com>
2017-04-18 08:47:45 -0700

    Generic error codes

--- 404.php
+++ 404.php
@@ -2,181 +2,25 @@
 <html lang="en">
   <head>
     <title>
-      HTTP 404 NOT FOUND
+      <?php
+      $title = "HTTP 404 NOT FOUND";
+      echo $title;
+      ?>
     </title>
-    <meta charset="utf-8"/>
-    <meta name="theme-color" content="#446f74"/>
-    <meta name="viewport" content="width=device-width, minimum-scale=1,initial-scale=1" />
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-
-
-    <meta name="description" content="The American Society of Mammalogists' searchable database of mammals across the world. Species accounts, photos, geolocation, and more."/>
-
     <base href="http://localhost/~Philip/" />
-
-
-    <link rel='shortcut icon' type='image/vnd.microsoft.icon' sizes='32x32' href='assets/favicon.ico' />
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon16.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon32.png" />
-    <link rel="icon" type="image/png" sizes="64x64" href="assets/favicon64.png" />
-    <link rel="icon" type="image/png" sizes="128x128" href="assets/favicon128.png" />
-    <link rel="icon" type="image/png" sizes="256x254" href="assets/favicon256.png" />
-    <link rel="icon" type="image/png" sizes="512x512" href="assets/favicon512.png" />
-    <link rel="icon" type="image/png" sizes="1024x1024" href="assets/favicon1024.png" />
-    <link rel="icon" type="image/png" sizes="2048x2048" href="assets/favicon2048.png" />
-    <link rel="icon" type="image/svg+xml" href="assets/logo.svg" />
-    <link rel="manifest" href="/manifest.json" />
-
-    <link href="css/main.min.css" rel='stylesheet' type='text/css'/>
-
-
-    <!-- Icons -->
-    <link href="css/glyphicons-bootstrap.css" rel='stylesheet' type='text/css'/>
-    <link href="css/glyphicons.css" rel='stylesheet' type='text/css'/>
-    <link rel="stylesheet" type="text/css" href="bower_components/octicons/octicons/octicons.css"/>
-
-    <link href='https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en' rel='stylesheet' type='text/css'>
-
-    <script  type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-    <script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
-
-    <link rel="import" href="bower_components/polymer/polymer.html"/>
-    <link rel="import" href="bower_components/font-roboto/roboto.html"/>
-    <link rel="import" href="bower_components/iron-icons/iron-icons.html"/>
-    <link rel="import" href="bower_components/iron-icons/image-icons.html"/>
-    <link rel="import" href="bower_components/iron-icons/social-icons.html"/>
-    <link rel="import" href="bower_components/iron-icons/communication-icons.html"/>
-    <link rel="import" href="bower_components/iron-icons/maps-icons.html"/>
-
-    <link rel="import" href="bower_components/paper-toggle-button/paper-toggle-button.html"/>
-    <link rel="import" href="bower_components/paper-toast/paper-toast.html"/>
-    <link rel="import" href="bower_components/paper-input/paper-input.html"/>
-    <link rel="import" href="bower_components/paper-spinner/paper-spinner.html"/>
-    <link rel="import" href="bower_components/paper-menu/paper-menu.html"/>
-    <link rel="import" href="bower_components/paper-menu-button/paper-menu-button.html"/>
-    <link rel="import" href="bower_components/paper-dialog/paper-dialog.html"/>
-    <link rel="import" href="bower_components/paper-dialog-scrollable/paper-dialog-scrollable.html"/>
-    <link rel="import" href="bower_components/paper-button/paper-button.html"/>
-    <link rel="import" href="bower_components/paper-radio-button/paper-radio-button.html"/>
-    <link rel="import" href="bower_components/paper-radio-group/paper-radio-group.html"/>
-    <link rel="import" href="bower_components/paper-icon-button/paper-icon-button.html"/>
-    <link rel="import" href="bower_components/paper-fab/paper-fab.html"/>
-    <link rel="import" href="bower_components/paper-item/paper-item.html"/>
-    <link rel="import" href="bower_components/paper-icon-button/paper-icon-button.html"/>
-    <link rel="import" href="bower_components/marked-element/marked-element.html"/>
-
-    <link rel="import" href="bower_components/neon-animation/neon-animation.html"/>
-
-    <link rel="import" href="polymer-elements/copyright-statement.html"/>
-    <link rel="import" href="polymer-elements/glyphicon-social-icons.html"/>
-
-
-    <script type="text/javascript" src="js/purl.min.js"></script>
-    <script type="text/javascript" src="js/xmlToJSON.min.js"></script>
-    <script type="text/javascript" src="bower_components/js-base64/base64.min.js"></script>
-    <script type="text/javascript" src="bower_components/picturefill/dist/picturefill.min.js"></script>
-    <script src="bower_components/imagelightbox/dist/imagelightbox.min.js"></script>
-    <script src="bower_components/markdown/lib/markdown.js"></script>
-    <script type="text/javascript" src="js/c.min.js"></script>
-
-
-    <!-- Meta descriptors -->
-
-    <!-- Twitter cards -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:site" content="@mammalogists">
-    <meta name="twitter:creator" content="@mammalogists">
-    <meta name="twitter:title" content="American Society Of Mammalogists - Species Account Database">
-    <meta name="twitter:description" content="The American Society of Mammalogists' searchable database of mammals across the world. Species accounts, photos, geolocation, and more.">
-    <meta property="twitter:image" content="https://mammaldiversity.org/species_photos/Tragelaphus_eurycerus.jpg" />
-    <meta property="twitter:image:width" content="4071"/>
-    <meta property="twitter:image:height" content="3456"/>
-    <!-- Facebook OG tags -->
-    <meta property="og:title"
-          content="American Society Of Mammalogists - Species Account Database" />
-    <meta property="og:site_name" content="American Society of Mammalogists"/>
-    <meta property="og:url"
-          content="https://mammaldiversity.org/" />
-    <meta property="og:image" content="https://mammaldiversity.org/assets/favicon1024.png" />
-    <meta property="og:image:width" content="1024"/>
-    <meta property="og:image:height" content="1024"/>
-    <meta property="og:description" content="The American Society of Mammalogists' searchable database of mammals across the world. Species accounts, photos, geolocation, and more."/>
-    <!-- <meta property="fb:page" content="970367112996034" /> -->
-    <!-- <meta property="fb:app_id" content="974878739261435" /> -->
-
-    <link href="https://plus.google.com/107225054672743265667" rel="publisher" />
-
-    <!-- Schema.org -->
-    <script type="application/ld+json">
-      { "@context" : "http://schema.org",
-      "@type" : "Organization",
-      "name" : "ASM Species Account Database",
-      "url" : "https://mammaldiversity.org/",
-      "logo" : "https://mammaldiversity.org/assets/favicon2048.png",
-      "image" : "https://mammaldiversity.org/species_photos/Tragelaphus_eurycerus.jpg",
-      "description" : "The American Society of Mammalogists' searchable database of mammals across the world. Species accounts, photos, geolocation, and more."
-      }
-    </script>
-
-
-    <script type="text/javascript">
-      (function(){var p=[],w=window,d=document,e=f=0;p.push('ua='+encodeURIComponent(navigator.userAgent));e|=w.ActiveXObject?1:0;e|=w.opera?2:0;e|=w.chrome?4:0;
-      e|='getBoxObjectFor' in d || 'mozInnerScreenX' in w?8:0;e|=('WebKitCSSMatrix' in w||'WebKitPoint' in w||'webkitStorageInfo' in w||'webkitURL' in w)?16:0;
-      e|=(e&16&&({}.toString).toString().indexOf("\n")===-1)?32:0;p.push('e='+e);f|='sandbox' in d.createElement('iframe')?1:0;f|='WebSocket' in w?2:0;
-      f|=w.Worker?4:0;f|=w.applicationCache?8:0;f|=w.history && history.pushState?16:0;f|=d.documentElement.webkitRequestFullScreen?32:0;f|='FileReader' in w?64:0;
-      p.push('f='+f);p.push('r='+Math.random().toString(36).substring(7));p.push('w='+screen.width);p.push('h='+screen.height);var s=d.createElement('script');
-      s.src='//mammaldiversity.org/bower_components/whichbrowser/detect.php?' + p.join('&');d.getElementsByTagName('head')[0].appendChild(s);})();
-      /*window.onerror = function(e) {
-      console.warn("Error thrown: "+e);
-      return true;
-      }*/
-    </script>
-
-
+    <?php include_once dirname(__FILE__)."/modular/header.php"; ?>
 
   </head>
-  <body fullbleed vertical layout class='container-fluid'>
-    <header id="header-bar" class="fixed-bar clearfix row">
-      <div class="logo-container col-xs-2">
-        <div class="square-object">
-          <div class="square tile">
-            <img src="assets/favicon512.png" alt="ASM logo" class="content click" data-href="http://mammology.org/" data-newtab="true" title='Visit Mammalogy.org' data-toggle='tooltip' data-placement='bottom'/>
-          </div>
-        </div>
-      </div>
-      <p class="col-xs-10 text-right">
-        <span class="logged-in-values" hidden>
-          Logged in as <span class="fill-user-fullname header-bar-user-name"></span>
-        </span>
-        <paper-icon-button icon="icons:dashboard" class="click logged-in-values" data-href="https://mammaldiversity.org/admin-page.html" data-toggle="tooltip" title="Administration Dashboard" data-placement="bottom" hidden> </paper-icon-button>
-        <paper-icon-button icon='icons:settings-applications' class="click logged-in-values" data-href="https://mammaldiversity.org/admin" data-toggle="tooltip" title="Account Settings" data-placement="bottom" hidden></paper-icon-button>
-        <paper-icon-button icon="glyphicon-social:playing-dice" class="click" data-fn="getRandomEntry" data-toggle="tooltip" title="Random Entry" data-placement="bottom"></paper-icon-button>
-      </p>
-    </header>
-    <main class='row'> 
-  <div class="col-xs-12">
-    <img src="https://media.giphy.com/media/9J7tdYltWyXIY/giphy.gif" alt="HTTP 404 errror" />  
-  </div>
+  <?php
+    require_once dirname(__FILE__)."/modular/bodyFrame.php";
+    echo $bodyOpen;
+    ?>
+    <h1 class="col-xs-12">Lost?</h1>
+    <div class="col-xs-12 error-image">
+      <img src="https://media.giphy.com/media/9J7tdYltWyXIY/giphy.gif" alt="HTTP 404 errror" />
+    </div>
   <p class="col-xs-12">
-    Nope, nothing there. Or, in web parlance, <code>404 NOT FOUND</code>
+    Nope, nothing there. Or, in web parlance, <code>404 NOT FOUND</code>. Try one of the links in the header to get you to someplace that actually exists :-)
   </p>
-  
-    </main>
-
-    <footer class="row hidden-xs">
-      <div class="copyright col-xs-8 col-lg-9">
-        <copyright-statement copyrightStart="2017">American Society of Mammalogists</copyright-statement>
-      </div>
-      <div class="col-sm-2" id="git-footer">
-        <paper-icon-button icon="glyphicon-social:github" class="click" data-url="https://github.com/tigerhawkvok/asm-mammal-database" data-toggle="tooltip" title="Visit us on GitHub"></paper-icon-button>
-      </div>
-      <div class="col-lg-1 col-sm-2" id="bug-footer">
-        <paper-icon-button icon="icons:bug-report" class="click" data-url="https://github.com/tigerhawkvok/asm-mammal-database/issues/new" data-toggle="tooltip" title="Report a bug"></paper-icon-button>
-      </div>
-    </footer>
-
-
-</body></html>
+  <?php echo $bodyClose; ?>
+</html>

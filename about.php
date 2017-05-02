abad65247e27df2aafc9f920d9096c15adf13965
Philip Kahn <tigerhawkvok@gmail.com>
2017-05-02 09:55:54 -0700

    More about

--- about.php
+++ about.php
@@ -12,7 +12,7 @@ if ($show_debug === true) {
 }
 else {
 	# Rigorously avoid errors in production
-					  ini_set('display_errors', 0);
+												  ini_set('display_errors', 0);
 }
 include_once dirname(__FILE__)."/CONFIG.php";
 ?>
@@ -20,7 +20,7 @@ include_once dirname(__FILE__)."/CONFIG.php";
   <head>
     <?php
       $title = "About the Species Account Database";
-      $pageDescription = "About the Species Account Database";
+$pageDescription = "About the Species Account Database";
 ?>
     <title><?php echo $title;
 ?></title>
@@ -46,12 +46,27 @@ echo $bodyOpen;
       <h1 id="title" class="col-xs-12">
         About the  Database
       </h1>
+      <div class="col-xs-12 clearfix">
+      <img src="assets/asm-collage-lowres.jpg" class="pull-right" alt="ASM collage"/>
       <p>
+      Development for this work was funded by the NSF Vertlife grant and commissioned by the <a href="http://www.mammalogy.org/about-asm">American Society of Mammalogists</a>.
       </p>
+      <p>
+        The American Society of Mammalogists (ASM) was founded in 1919. Its primary purpose is to encourage the study of mammals and professions studying mammals. There are over 2,500 members in the composed society,  primarily professional scientists who emphasize the importance of public policy and education.
+      </p>
+      <p>
+      There are several ASM meetings held each year and the society maintains several publications. The flagship publication of the ASM is <i><a href="https://academic.oup.com/jmammal" class="newwindow">The Journal of Mammalogy</a></i>, published six times a year. Other journals include <i>Special Publications</i> and <i><a href="https://academic.oup.com/mspecies" class="newwindow">Mammalian Species</a></i>.
+      </p>
+      </div>
+      <h2 class="col-xs-12">API</h2>
+      <p class="col-xs-12">
+        Please view <a href="<?php echo $gitUrl; ?>/blob/master/README.md#api"><code>README.md</code> in the Github repository</a> for information on how to use our public taxon API. The API endpoint can be found at <code><a href="https://mammaldiversity.org/api/q=ursus+arctos" class="newwindow">https://mammaldiversity.org/api</a></code>.
+        </p>
       <h2 class="col-xs-12">Legal Notices</h2>
       <h3 class="col-xs-12">Application</h3>
       <p class="col-xs-12">
-      This software is released under the <a href="https://choosealicense.com/licenses/gpl-3.0/" class="newwindow">GNU GPL v3 license</a>, and the source code is available <a href="<?php echo $gitUrl; ?>" class="newwindow">on Github</a>.
+      This software is released under the <a href="https://choosealicense.com/licenses/gpl-3.0/" class="newwindow">GNU GPL v3 license</a>, and the source code is available <a href="<?php echo $gitUrl;
+?>" class="newwindow">on Github</a>. Licenses for dependencies and are either included in the directory for the package, or in the file <code>PACKAGE-LICENSES</code> in the respository.
       </p>
       <h3 class="col-xs-12">Images</h3>
       <p class="col-xs-12">

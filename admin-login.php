b734cd47182b7582418ae6f8aedd63e528b43a0c
Philip Kahn <tigerhawkvok@gmail.com>
2017-05-17 14:03:59 -0700

    Squash errors explicitly

--- admin-login.php
+++ admin-login.php
@@ -2,19 +2,32 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
   <head>
     <?php
-      $debug = false;
-      $title = "ASM SADB Admin Login";
-      $pageDescription = "The American Society of Mammalogists' searchable database of mammals across the world. Species accounts, photos, geolocation, and more.";
-      ?>
+    # $show_debug = true;
+    if ($show_debug === true) {
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        error_log('Admin login is running in debug mode!');
+        $debug = true; # compat
+    } else {
+        # Rigorously avoid errors in production
+        ini_set('display_errors', 0);
+    }
+    if (!isset($print_login_state)) {
+        $print_login_state = true;
+    }
+
+    $title = "ASM SADB Admin Login";
+    $pageDescription = "The American Society of Mammalogists' searchable database of mammals across the world. Species accounts, photos, geolocation, and more.";
+    ?>
     <title><?php echo $title; ?></title>
 
     <?php include_once dirname(__FILE__)."/modular/header.php"; ?>
 
   </head>
     <?php
-       require dirname(__FILE__)."/modular/bodyFrame.php";
-       echo $bodyOpen;
-       if ($debug) {
+    require dirname(__FILE__)."/modular/bodyFrame.php";
+    echo $bodyOpen;
+    if ($show_debug === true) {
            echo "<div class='alert alert-danger'><strong>Warning:</strong> Debugging is enabled on admin-login.php</div>";
            // ini_set('error_log', '/usr/local/web/amphibian_disease/error-admin.log');
            ini_set('display_errors', 1);
@@ -27,9 +40,9 @@
            // $decrypted = openssl_decrypt($encrypted, "AES-256-CBC", $pass);
            // $encrypt_test = "<pre>OpenSSL Encrypt Test: \n\n $methods \n\n $encrypted \n\n $decrypted</pre>";
            // echo $encrypt_test;
-       }
-       require 'CONFIG.php';
-       require 'admin/login.php';
-       echo $login_output;
-       echo $bodyClose;
-     ?>
+    }
+    require 'CONFIG.php';
+    require 'admin/login.php';
+    echo $login_output;
+    echo $bodyClose;
+    ?>

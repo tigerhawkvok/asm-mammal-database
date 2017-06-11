5a0cd02ad42e387bdee5af286dd14da5d87985f5
Philip Kahn <tigerhawkvok@gmail.com>
2017-06-11 15:53:36 -0700

    re-enable a more mobile-friendly footer

--- modular/footer.php
+++ modular/footer.php
@@ -1,11 +1,11 @@
 <?php
 include dirname(__FILE__) . "/../CONFIG.php";
 ?>
-<footer class="row hidden-xs">
-  <div class="copyright col-xs-6 col-lg-9">
-    <copyright-statement copyrightStart="2017">American Society of Mammalogists</copyright-statement>
+<footer class="row">
+  <div class="copyright col-xs-5 col-lg-9">
+    <copyright-statement copyrightStart="2017"><span class="hidden-xs">American Society of Mammalogists</span><span class="visible-xs">ASM</span></copyright-statement>
   </div>
-  <div class="col-xs-4 col-lg-2" id="git-footer">
+  <div class="col-xs-5 col-lg-2" id="git-footer">
     <paper-icon-button icon="glyphicon-social:github" class="click" data-url="<?php echo $gitUrl; ?>" data-toggle="tooltip" title="Visit us on GitHub"></paper-icon-button>
   </div>
   <div class="col-xs-2 col-lg-1" id="bug-footer">
